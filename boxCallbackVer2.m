028b2a35392aed84c99c3ee4ae2afb1e986f03f9
crhammon <crhammon@users.noreply.github.com>
2017-03-15 08:51:55 -0700

    Add files via upload

--- boxCallbackVer2.m
+++ boxCallbackVer2.m
@@ -14,15 +14,15 @@ pos=clicked.Position
 xcoor=ceil((pos(2)+1)/15)
 ycoor=ceil((pos(1)+1)/15)
 
-if evalin('base','PlotChoice')=='Plot 1'
+if evalin('base','PlotChoice')=='Miami, FL'
     Plot1Info=evalin('base','Plot1Info');
     Plot1Info(xcoor,ycoor)=0;
     assignin('base','Plot1Info',Plot1Info);
-elseif evalin('base','PlotChoice')=='Plot 2'
+elseif evalin('base','PlotChoice')=='Miami, FL'
     Plot2Info=evalin('base','Plot2Info');
     Plot2Info(xcoor,ycoor)=0;
     assignin('base','Plot2Info',Plot2Info);
-elseif evalin('base','PlotChoice')=='Plot 3'
+elseif evalin('base','PlotChoice')=='Miami, FL'
     Plot3Info=evalin('base','Plot3Info');
     Plot3Info(xcoor,ycoor)=0;
     assignin('base','Plot3Info',Plot3Info);
